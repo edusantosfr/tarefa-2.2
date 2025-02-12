5c3349810b430fcebae41d9bd2e1c8b44d2b4c1c
edusantosfr <70604190+edusantosfr@users.noreply.github.com>
2025-02-12 16:43:40 -0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import { Container } from './components/Container';
+import { FilmsTable } from './components/Container/index';
+import { SearchBar } from './components/SearchBar/index';
+import films from "./components/Container/251filmes807.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Container />
-      </header>
-    </div>
+const App: React.FC = () => {
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const searchFilm = films.filter((film) => 
+    film.nome.toLowerCase().includes(searchValue.toLowerCase())
   );
-}
+
+  return (
+      <div className="App">
+        <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+        <FilmsTable film={searchFilm} />
+      </div>
+  )
+};
 
 export default App;
--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,40 +1,37 @@
-import { useState, useEffect } from "react";
+import React from "react";
+import "./index.css";
 
-export function Container () {
-    const [count, setCount] = useState(0);
-    const [countB, setCountB] = useState(0);
-    const [user, setUser] = useState(1);
-
-    function getUserData () {
-
-        fetch('https://api.github.com/users/edusantosfr')
-        .then((res) => res.json())
-        .then((json) => setUser(json))
-    }
-
-    useEffect(()=> {
-        console.log("Executa quando renderiza")
-    })
-
-    useEffect(()=> {
-        console.log("Executa quando count altera")
-    },[count])
-
-    useEffect(()=> {
-        console.log(user)
-    },[user])
-
-    useEffect(() => {
-        getUserData()
-    },[])
-
-    return (
-        <div>
-            <button onClick={() => setCount((prevCount) => prevCount + 1)}>Adicona +</button>
-            <p>Contador: {count}</p>
-
-            <button onClick={() => setCountB((prevCount) => prevCount + 1)}>Adicona +</button>
-            <p>Contador B: {countB}</p>
-        </div>
-    )
+type Filme = {
+    id: number;
+    nome: string;
+    genero: string;
+    imagem: string;
 }
+
+type Props = {
+    film: Filme[];
+}
+
+export const FilmsTable: React.FC<Props> = ({ film }) => {
+    return (
+        <table border={1} className="table">
+            <thead className="tableHead">
+                <tr>
+                    <th><b><p>Banner</p></b></th>
+                    <th><b><p>Nome</p></b></th>
+                    <th><b><p>Gênero</p></b></th>
+                </tr>
+            </thead>
+            <tbody className="tableBody">
+                {film.length > 0 && film.map((element) => (
+                    <tr key={element.id} className="tableItems">
+                        <td><img src={element.imagem} alt="banner de filme" /></td>
+                        <td><h3>{element.nome}</h3></td>
+                        <td><p>{element.genero}</p></td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </table>
+    );
+};
